535cdcc129fcaf816be5d35970e9a538cadc2887
grassroadsZ <zys17666541107@163.com>
2021-03-31 20:08:19 +0800

    2021年3月30日18:29:18 第一次提交

--- apps/trade/models.py
+++ apps/trade/models.py
@@ -9,7 +9,7 @@ class DealConfigModels(models.Model):
         verbose_name = '交易配置'
         verbose_name_plural = verbose_name
 
-    id = models.IntegerField(primary_key=True, verbose_name="交易id")
+    id = models.IntegerField(primary_key=True, verbose_name="交易对id")
     coin_type = models.CharField(max_length=12, verbose_name="交易对", help_text="交易对,如: BTCUSDT")
     strategy = models.ForeignKey(verbose_name="策略", help_text="使用的策略", )
 
@@ -23,6 +23,21 @@ class StrategyBaseModels(models.Model):
         verbose_name = '策略基础配置'
         verbose_name_plural = verbose_name
 
-    strategy_id = models.IntegerField(primary_key=True, verbose_name="交易id")
+    strategy_id = models.IntegerField(primary_key=True, verbose_name="策略id")
     coin_type = models.CharField(max_length=12, verbose_name="交易对", help_text="交易对,如: BTCUSDT")
-    strategy = models.ForeignKey(verbose_name="策略", help_text="使用的策略", )
+    strategy = models.CharField(verbose_name="策略", help_text="策略")
+
+    def __str__(self):
+        return self.strategy_id
+
+
+class StrategyConfigModels(models.Model):
+    class Meta:
+        db_table = "td_strategy_config"
+        verbose_name = '策略配置'
+        verbose_name_plural = verbose_name
+
+    strategy_config_id = models.IntegerField(primary_key=True, verbose_name="具体策略id")
+    strategy_key = models.ForeignKey(StrategyBaseModels, on_delete=models.CASCADE, verbose_name="策略")
+    param_key = models.CharField(max_length=12, verbose_name="策略key", help_text="策略key,如: ")
+    # strategy = models.ForeignKey(DealConfigModels, on_delete=models.CASCADE, verbose_name="策略", help_text="策略" )
